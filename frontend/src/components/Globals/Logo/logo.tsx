b3ef882ffce2a3824e7dcc117de9092271580e61
Matthew Ediger <edigermatthew@gmail.com>
2025-03-06 17:02:45 -0700

    Change spacing

--- frontend/src/components/Globals/Logo/logo.tsx
+++ frontend/src/components/Globals/Logo/logo.tsx
@@ -2,11 +2,11 @@ import Link from "next/link";
 import Logo from "@/public/logo.svg";
 
 export default function LogoAndLink() {
-    return (
-        <div className="mr-2.5 py-2">
-            <Link href="/">
-                <Logo aria-label="Logo" className="w-40 md:w-44" />
-            </Link>
-        </div>
-    )
+  return (
+    <div className="mr-2.5 py-2">
+      <Link href="/">
+        <Logo aria-label="Logo" className="w-40 md:w-44" />
+      </Link>
+    </div>
+  )
 }
