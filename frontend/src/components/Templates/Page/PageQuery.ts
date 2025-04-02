18ade83a31285689f0d98724c5b631d62bccda10
Matthew Ediger <edigermatthew@gmail.com>
2025-04-01 19:57:17 -0600

    Add block components

--- frontend/src/components/Templates/Page/PageQuery.ts
+++ frontend/src/components/Templates/Page/PageQuery.ts
@@ -4,21 +4,7 @@ export const PageQuery = gql`
   query PageQuery($id: ID!, $preview: Boolean = false) {
     page(id: $id, idType: DATABASE_ID, asPreview: $preview) {
       title
-      content
-      blocks {
-        name
-        originalContent
-        saveContent
-        dynamicContent
-      }
       blocksJSON
-    },
-    pages {
-      edges {
-        node {
-          blocksJSON
-        }
-      }
     }
   }
 `;
--- frontend/src/components/Templates/Page/PageTemplate.tsx
+++ frontend/src/components/Templates/Page/PageTemplate.tsx
@@ -2,6 +2,7 @@ import { print } from "graphql/language/printer";
 import { ContentNode, Page } from "@/gql/graphql";
 import { fetchGraphQL } from "@/utils/fetchGraphQL";
 import { PageQuery } from "./PageQuery";
+import { getBlockComponents } from "@/utils/getBlockComponents";
 
 interface TemplateProps {
   node: ContentNode;
@@ -11,33 +12,20 @@ export default async function PageTemplate({ node }: TemplateProps) {
   const { page } = await fetchGraphQL<{ page: Page }>(print(PageQuery), {
     id: node.databaseId,
   });
-  const { pages } = await fetchGraphQL<{ page: Page }>(print(PageQuery), {
-    id: node.databaseId,
-  });
-
-  const { name, blockType, attributes } = JSON.parse(page.blocksJSON)[0] || {};
-
+  const blockComponents = await getBlockComponents(page.blocksJSON);
 
   return (
     <main className="py-8 md:py-12">
       <header className="container mx-auto mb-8 px-4">
         <h1 className="text-3xl md:text-4xl lg:text-6xl">{page.title}</h1>
       </header>
-      <div
-        className="entry-content"
-        // dangerouslySetInnerHTML={{ __html: page.content }}
-      ></div>
-      { console.log( page.blocks )}
-      { console.log( pages )}
-      {
-      /* <pre>
-        {blocksJson.map((block) => (
-          console.log(block),
-          <div key={block.clientId}>
-            <pre>{block.name}</pre>
-          </div>
-        ))}
-      </pre> */}
+      { blockComponents.length > 0 ? (
+        <div className="entry-content">
+          {blockComponents}
+        </div>
+      ): (
+        <p className="text-center text-lg text-gray-500">No content available.</p>
+      )}
     </main>
   );
 }
