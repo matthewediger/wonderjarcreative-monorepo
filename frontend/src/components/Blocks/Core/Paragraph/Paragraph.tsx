7887cfffde830dd984be736cdf490a3dc3420eda
Matthew Ediger <edigermatthew@gmail.com>
2025-05-24 22:39:21 -0600

    fix: use new utility classes

--- frontend/src/components/Blocks/Core/Paragraph/Paragraph.tsx
+++ frontend/src/components/Blocks/Core/Paragraph/Paragraph.tsx
@@ -1,15 +1,28 @@
-export default function Paragraph(
-  { key, attributes, originalContent, innerBlocks }: 
-  { key: any, attributes: { anchor?: string, className?: string }; originalContent?: string, innerBlocks?: any[] | null },
-) {
-  const { anchor, className } = attributes;
+import { Maybe, CoreParagraphBlockAttributes } from '@/gql/graphql';
+import { getBlockClasses, getBlockStyle } from '@/utils/getBlockComponents';
+
+type ParagraphProps = {
+  attributes: CoreParagraphBlockAttributes;
+  dynamicContent?: Maybe<string> | undefined;
+  key: number;
+  originalContent?: Maybe<string> | undefined;
+  saveContent?: Maybe<string> | undefined;
+};
+
+export default function Paragraph({attributes, dynamicContent, key, originalContent, saveContent}: ParagraphProps) {
+  const { anchor, style } = attributes;
+  const blockClasses = getBlockClasses(attributes, 'wp-block-paragraph');
+  const blockStyle = getBlockStyle(style);
+
   return (
     <p
       key={key}
       {...(anchor && { id: anchor })}
-      className={`wp-block-paragraph ${className || ""}`}
-    >
-      { originalContent?.replace(/<p[^>]*>|<\/p>/g, '') || "" }
-    </p>
+      className={blockClasses}
+      {...(style && { style: blockStyle })}
+      dangerouslySetInnerHTML={{
+        __html: (dynamicContent || saveContent || originalContent)?.replace(/<p[^>]*>|<\/p>/g, '') || '',
+      }}
+    />
   );
 }
